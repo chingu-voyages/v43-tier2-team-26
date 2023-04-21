c5ff35c4586100c4fe52680c3ec47e9c69b7aa21
hanna <hannawkozak@gmail.com>
2023-04-21 21:50:41 +0100

    style:add responsiveness to landing page

--- whenwhere/src/pages/landingpage.js
+++ whenwhere/src/pages/landingpage.js
@@ -10,34 +10,32 @@ const LandingPage = () => {
   };
   return (
     <div className="App">
-      <header>
-        <h1>
-          Meeting Scheduling <br></br>Made Easy
-        </h1>
-      </header>
-      <main>
-        <section>
-          <h5>
-            Real time meeting scheduling<br></br> with text reminder
-          </h5>
+      <main className="container position-relative">
+        <section className="col col-lg-6 d-flex flex-column gap-5 justify-content-center align-items-center align-middle min-vh-100 text-center ">
+          <h1>
+            Meeting Scheduling <br></br>Made Easy
+          </h1>
+          <h5>WhereWhen helps you find the best time for a group to meet</h5>
           <div className="button">
             <button className="button" onClick={redirectToCreateEventPage}>
               Schedule Meeting
             </button>
           </div>
         </section>
-        <div className="bman-container">
-          <div className="bman"></div>
-        </div>
-        <div className="hajia-container">
-          <div className="hajia"></div>
-        </div>
-        <div className="richman-container">
-          <div className="richman"></div>
-        </div>
-        <div className="suitman-container">
-          <div className="suitman"></div>
-        </div>
+        <section className="d-none d-lg-block col-md-6 section-gallery align-items-center align-middle">
+          <div className="bman-container">
+            <div className="bman"></div>
+          </div>
+          <div className="hajia-container">
+            <div className="hajia"></div>
+          </div>
+          <div className="richman-container">
+            <div className="richman"></div>
+          </div>
+          <div className="suitman-container">
+            <div className="suitman"></div>
+          </div>
+        </section>
       </main>
       <footer>
         <p>Copyright &copy; WhenWhere 2023</p>
